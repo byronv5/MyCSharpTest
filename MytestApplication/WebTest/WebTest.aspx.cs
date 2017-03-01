90b65f341e90e3035201ef27baac289dcc15c4ac
byronv5 <byeshore@qq.com>
2017-03-01 16:53:21 +0800

    备注
    备注说明ExceptionLess

--- MytestApplication/WebTest/WebTest.aspx.cs
+++ MytestApplication/WebTest/WebTest.aspx.cs
@@ -2,6 +2,7 @@
 using Common.AbilityInterface;
 using Common.AbilityInterface.InterfaceLibrary;
 using Newtonsoft.Json.Linq;
+using Exceptionless;
 
 namespace WebTest
 {
@@ -14,15 +15,26 @@ namespace WebTest
 
         protected void Btn_test_Click(object sender, EventArgs e)
         {
-            //查询客户信息--能力接口
-            Customer reqCustomer = new Customer
+            try
             {
-                DeviceNo = "18101767389",
-                DeviceType = "18"
-            };
-            Context ct = new Context(reqCustomer);
-            string rst = ct.Excute();//返回结果为json
-            JObject jObject = JObject.Parse(rst);//反序列化为object
+                //查询客户信息--能力接口
+                Customer reqCustomer = new Customer
+                {
+                    DeviceNo = "18101767389",
+                    DeviceType = "18"
+                };
+                Context ct = new Context(reqCustomer);
+                string rst = ct.Excute();//返回结果为json
+                JObject jObject = JObject.Parse(rst);//反序列化为object
+                if (jObject["ErrCode"].ToString() != "0000")
+                {
+                    throw new ApplicationException(jObject["ErrMsg"].ToString());
+                }                
+            }
+            catch (Exception ex)
+            {
+                ex.ToExceptionless().Submit();//提交异常到ExceptionLess
+            }         
         }
     }
 }
