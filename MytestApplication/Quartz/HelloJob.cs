bb2a3fb431dff3bd777828275bcb523618ea48a9
byronv5 <byeshore@qq.com>
2018-02-05 17:43:16 +0800

    QuartzNet特性DisallowConcurrentExecution

--- MytestApplication/Quartz/HelloJob.cs
+++ MytestApplication/Quartz/HelloJob.cs
@@ -1,8 +1,10 @@
 ﻿using Quartz;
 using System;
+using System.Threading;
 
 namespace QuartzSample
 {
+    [DisallowConcurrentExecution]//这个特性使得任务单线程运行，比如前一个任务计划还未执行完，后来的只能等
     public class HelloJob : IJob
     {
         /// <summary>
@@ -12,6 +14,7 @@ namespace QuartzSample
         public void Execute(IJobExecutionContext context)
         {
             //dosth：我们的计划任务
+            Thread.Sleep(6000);//验证DisallowConcurrentExecution
             Console.WriteLine("Hello " + DateTime.Now.ToString());
         }
     }
