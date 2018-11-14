f3faad909e735892154c71b604b5425312423f2a
白小龙 <byeshore@qq.com>
2018-11-14 23:10:29 +0800

    单例模式

---
+++ MytestApplication/Singleton/HungrySingleton.cs
@@ -0,0 +1,15 @@
+﻿namespace Singleton
+{
+    /// <summary>
+    /// 饿汉模式
+    /// </summary>
+    public class HungrySingleton
+    {
+        private static readonly HungrySingleton instance = new HungrySingleton();
+        private HungrySingleton() { }
+        public static HungrySingleton GetInstance()
+        {
+            return instance;
+        }
+    }
+}
---
+++ MytestApplication/Singleton/LazySingleton.cs
@@ -0,0 +1,32 @@
+﻿namespace Singleton
+{
+    /// <summary>
+    /// 懒汉模式
+    /// </summary>
+    public class LazySingleton
+    {
+        private static LazySingleton instance;
+        /// <summary>
+        /// 锁：保证线程安全
+        /// </summary>
+        private static readonly object lockObj = new object();
+        /// <summary>
+        /// 私有构造防止继承、外部实例化
+        /// </summary>
+        private LazySingleton() { }
+        public static LazySingleton GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (lockObj)
+                {
+                    if (instance == null)
+                    {
+                        instance = new LazySingleton();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+}
